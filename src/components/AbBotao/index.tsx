c8b5f782b45e421b28faf6328018a49c62c103ad
root <root@OFNTB202414.>
2025-05-28 16:57:25 -0300

    Styled-Components

--- src/components/AbBotao/index.tsx
+++ src/components/AbBotao/index.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import styled from "styled-components";
+
+const BotaoEstilizado = styled.button`
+    background: #EB9B00;
+    padding: 16px 32px;
+    border: #EB9B00 solid 2px;
+    color: #FFFFFF;
+    font-size: 20px;
+    cursor: pointer;
+    &:hover {
+        background: #B87900;
+        border: #B87900 solid 2px;
+    }
+`
+
 
 export const AbBotao = () => {
     return (
-        <button>Clique aqui!</button>
+        <BotaoEstilizado>Clique aqui!</BotaoEstilizado>
     )
 }
